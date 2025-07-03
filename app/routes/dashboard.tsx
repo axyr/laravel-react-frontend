53cbc48268f6601b7baa81892a21a67882e631e6
Martijn van Nieuwenhoven <martijn@novat.io>
2025-07-03 11:17:12 +0200

    add the awesome Laravel placeholder pattern.

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -2,6 +2,7 @@ import type { BreadcrumbItem } from '~/types'
 import AppLayout from '~/layouts/app-layout'
 import type { Route } from '../../.react-router/types/app/routes/+types/home'
 import { useSharedStore } from '~/stores/shared-store'
+import { PlaceholderPattern } from '~/components/ui/placeholder-pattern'
 
 const title = 'Dashboard';
 
@@ -22,13 +23,21 @@ export function meta({}: Route.MetaArgs) {
 export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <div className="flex flex-1 flex-col gap-4 p-4 overflow-x-auto">
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="bg-muted/50 aspect-video rounded-xl" />
-                    <div className="bg-muted/50 aspect-video rounded-xl" />
-                    <div className="bg-muted/50 aspect-video rounded-xl" />
+                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
                 </div>
-                <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                </div>
             </div>
         </AppLayout>
     )
