107dc2cb057a510b16145c523e11c8218cd19154
Martijn van Nieuwenhoven <martijn@novat.io>
2025-07-05 00:35:57 +0200

    add login image

--- app/layouts/auth/auth-split-layout.tsx
+++ app/layouts/auth/auth-split-layout.tsx
@@ -14,7 +14,13 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-                <div className="absolute inset-0 bg-zinc-900" />
+                <div className="absolute inset-0">
+                    <img
+                        src="/assets/login.webp"
+                        alt="Image"
+                        className="absolute inset-0 h-full w-full object-cover"
+                    />
+                </div>
                 <Link to="/" className="relative z-20 flex items-center text-lg font-medium">
                     <AppLogoIcon className="mr-2 size-8 fill-current text-white" />
                     {name}
