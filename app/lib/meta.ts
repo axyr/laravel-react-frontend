2aa33c0af4df3a51b9542ed2326e24aed1165a15
Martijn van Nieuwenhoven <martijn@novat.io>
2025-07-05 12:22:50 +0200

    Fix meta order of hooks

--- app/lib/meta.ts
+++ app/lib/meta.ts
@@ -1,7 +1,7 @@
 import { useSharedStore } from '~/stores/shared-store'
 
 export function setMeta(title: string, description?: string) {
-    const name = useSharedStore((s) => s.name)
+    const name = useSharedStore.getState().name
     return [
         { title: title ? `${title} - ${name}` : name },
         ...(description ? [{ name: 'description', content: description }] : []),
